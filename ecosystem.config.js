279fb2c0e1b0972b110d876dd721e0a7e41750fa
delorean7182 <alikhan.talipbayev@nu.edu.kz>
2026-01-14 12:17:11 +0500

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,16 +2,15 @@ module.exports = {
   apps: [
     {
       name: "praktikoffice",
-      script: "npm",
-      args: "start",
+      script: "npx",
+      args: "serve@latest out -l 3000",
       cwd: "./",
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: "1G",
+      max_memory_restart: "500M",
       env: {
         NODE_ENV: "production",
-        PORT: 3000,
       },
       error_file: "./logs/pm2-error.log",
       out_file: "./logs/pm2-out.log",
